bbac3a921c29722780edbcb6cbc38cccc847c34a
Timo Rantalainen <tjrantal@gmail.com>
2015-10-01 14:41:38 +1000

    Test succeeds reeding one record

--- ConvertGT3x/src/com/video4coach/LogBinReader.java
+++ ConvertGT3x/src/com/video4coach/LogBinReader.java
@@ -12,7 +12,7 @@ import java.io.*;
 import java.util.*;
 public class LogBinReader{
 	private byte[] data;
-	public ArrayList<ArrayList<Short>> accelerations;
+	public ArrayList<ArrayList<Short>> accelerations;	//Y, X, Z
 	public LogBinReader(String fileName){
 		try {
 			/*Read the file into memory (make sure you've got sufficient memory available...)*/
@@ -43,8 +43,48 @@ public class LogBinReader{
 			//Check that parsing is successful, should have 1E to indicate start of package
 			LogRecord logrecord = new LogRecord(data,pointer);
 			System.out.println("P "+pointer);
+			/*Extract accelerations here*/
 			if (logrecord.type == 0x00){
-				System.out.println("Found activity data, pointer "+pointer);
+				int valuesInRecord = (int) logrecord.payload.length*8/12;
+				int valuesExtracted = 0;
+				int direction = 0;	//Used to keep track of which dimension to extract
+				//Loop through the payload to extract the accelerometry values
+				short valueBits;
+				int payloadPointer = 0;
+				while (valuesExtracted <valuesInRecord){
+					//The values are 12 bit back-to-back -> 2 values take 3 bytes
+					if (valuesExtracted % 2 == 0){
+						valueBits =(short) (((logrecord.payload[payloadPointer]& 0xff)<<4)| ((logrecord.payload[payloadPointer+1] & 0xf0)>>4));
+					} else {
+						valueBits =(short) (((logrecord.payload[payloadPointer]& 0x0f)<<8)| (logrecord.payload[payloadPointer+1] & 0xff));
+						++payloadPointer;
+					}
+					++valuesExtracted;
+					++payloadPointer;
+					if (valueBits > 2047){
+						valueBits |=0xf000;	//Set the sign
+					}
+					//Assign the value to the correct dimension
+					accelerations.get(direction).add(valueBits);
+					++direction;
+					if (direction>2){
+						direction = 0;
+					}
+				}
+				for (int j = 0;j<accelerations.size();++j){
+						System.out.print(accelerations.get(j).size()+"\t");
+				}
+				System.out.println("\t");
+				System.out.println("ind\ty\tx\tz\t");
+				for (int i = 0;i<accelerations.get(0).size();++i){
+					System.out.print(i+"\t");
+					for (int j = 0;j<accelerations.size();++j){
+						System.out.print(accelerations.get(j).get(i)+"\t");
+					}
+					System.out.println("");
+				}
+				//System.out.println("Found activity data, pointer "+pointer+" values "+valuesInRecord);
+				
 				break;
 			}
 			pointer = logrecord.nextRecordPointer;
@@ -65,7 +105,7 @@ public class LogBinReader{
 			type		= data[pointer+1];
 			timeStamp	= (int) (((0xff & data[pointer+5])<<24) | ((0xff & data[pointer+4])<<16) | ((0xff & data[pointer+3])<<8) | (0xff & data[pointer+2]));
 			size		=(int) (((0xff & data[pointer+7])<<8) | (0xff & data[pointer+6]));
-			payload = Arrays.copyOfRange(data,pointer+8,pointer+8+payload);	//Copy the record payload
+			payload = Arrays.copyOfRange(data,pointer+8,pointer+8+size);	//Copy the record payload
 			checksum = data[pointer+8+size];
 			nextRecordPointer = pointer+8+size+1;
 		}
